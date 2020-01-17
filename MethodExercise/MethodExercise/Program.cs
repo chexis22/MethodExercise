376f3a3408b706fad7d6c97b1a337a3f02123610
Alexis Diaz <chexisss@gmail.com>
2020-01-17 10:53:17 -0600

    Part One

---
+++ MethodExercise/MethodExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+
+            var yourName = Console.ReadLine();
+
+            Console.WriteLine($"My name is {yourName}");
+
+            Console.WriteLine("What is your favorite color?");
+
+            var yourColor = Console.ReadLine();
+
+            Console.WriteLine($"My favorite color is {yourColor}!");
+
+            Console.WriteLine("What is your favorite animal?");
+
+            var yourAnimal = Console.ReadLine();
+
+            Console.WriteLine($"My favorite animal is a {yourAnimal}!");
+
+            Console.WriteLine("What is your favorite band?");
+
+            var yourBand = Console.ReadLine();
+
+            Console.WriteLine($"My favorite band is {yourBand}");
+        }
+    }
+}
