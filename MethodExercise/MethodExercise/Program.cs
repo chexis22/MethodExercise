02209b2d5ebe83a764985c13e1830f583ec4eea7
Alexis Diaz <chexisss@gmail.com>
2020-01-17 12:50:47 -0600

    Part tow and some

--- MethodExercise/MethodExercise/Program.cs
+++ MethodExercise/MethodExercise/Program.cs
@@ -6,6 +6,17 @@ namespace MethodExercise
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Add(4, 6));
+
+            Console.WriteLine(Multiply(10, 100));
+
+            Console.WriteLine(Multiply(100, 100) + " " + Add(20, 5));
+
+            Console.WriteLine("What is your gender?");
+
+            var answer = Console.ReadLine();
+            Girl(answer);
+
             Console.WriteLine("What is your name?");
 
             var yourName = Console.ReadLine();
@@ -32,5 +43,32 @@ namespace MethodExercise
 
         }
 
+        public static int Add(int x, int y)
+        {
+            var answer = x + y;
+            return answer;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            var answer = x * y;
+            return answer;
+        }
+
+        public static bool Girl(string gender)
+        {
+            bool response;
+            if (gender != "female")
+            {
+                response = false;
+            }
+            else
+            {
+                response = true;
+            }
+            Console.WriteLine(response);
+            return response;
+        }
+       
     }
 }
